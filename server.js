405400ae8a6afa06dfb25260d83335910936a3a6
Babu Imam <babubpx2762@gmail.com>
2020-08-25 20:07:40 -0700

    Formating functionality added to messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages.js');
 
 
 const app = express();
@@ -10,21 +11,22 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Chat Room Admin';
 //run when a client connects
 io.on('connection', socket => {
     
     //welcome current user
-    socket.emit('message', 'Welcome to Chatroom');
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatroom'));
     //Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user joined to the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user joined to the chat'));
     //When a client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message',formatMessage(botName, 'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        socket.io('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 
 });
